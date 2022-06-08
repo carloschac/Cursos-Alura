9e2227db0ef69b1d97daf456948f7840915ebb6d
Carlos Henrique <carloss.chac@gmail.com>
2022-06-08 12:01:57 -0300

    commit

---
+++ JavaScript-BackEnd/let-var-const/README.md
@@ -0,0 +1,5 @@
+// Por motivos de segurança utilizamos o let ao invés do var, porque em um código de grande volume podemos repitir o VAR por engano, mas não o LET
+
+var = Varíavel solta porém pouca segura e não recomendada
+let = Deve ser sempre declarada antes de ser utilizada, mas continua sendo uma varíavel solta.
+const = Varíavel constante, o valor não pode ser alterado e deve ser declarada antes. //
---
+++ JavaScript-BackEnd/truthy-falsy/script.js
@@ -0,0 +1,30 @@
+// boolean
+const usuarioLogado = true
+const contapaga = false
+
+//truthy ou falsy
+
+//0 => false
+//1 => true
+
+console.log(0 == false)
+console.log('' == false)
+console.log(1 == true)
+
+//null == Vazio ou nada
+//undefined
+
+let minhaVar
+let varNull = null
+
+console.log(typeof minhaVar)
+console.log(typeof varNull)
+//object - Está retornando como object ao invés de Null devido a um bug do Javascript
+
+let numero = 3
+let texto = 'Alura'
+
+console.log(typeof numero)
+console.log(typeof texto)
+
+//typeof é palavra chave para perguntar para o JS o tipo de varíavel
