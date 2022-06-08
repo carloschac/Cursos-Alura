ddc47e2ae08c9c23df55fe60d02a2e3c3611c9b3
Carlos Henrique <carloss.chac@gmail.com>
2022-06-08 12:16:51 -0300

    Update

--- JavaScript-BackEnd/conversoes/script.js
+++ JavaScript-BackEnd/conversoes/script.js
@@ -5,6 +5,9 @@
 const numero = 456
 const numeroString = '456'
 
-console.log(numero === numeroString)
+//Number()
+//String()
+//Sem o parseInt o código iria concatenar apenas, e ficar 456456, com o parseInt convertemos o numeroString para número inteiro, dessa forma o código soma ao invés de concatenar.
+console.log(numero + parseInt(numeroString))
 
 //conversão explícita
