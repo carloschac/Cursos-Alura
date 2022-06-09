75befe91469e6c0c8bc9f36b74d648ea9f5c6ec4
Carlos Henrique <carloss.chac@gmail.com>
2022-06-09 16:46:52 -0300

    Update

---
+++ JavaScript-BackEnd/Aula03/Console-api/console.js
@@ -0,0 +1,11 @@
+//log -> registro
+
+const minhaVar = true
+
+console.log(245)
+console.log('texto')
+console.log(minhaVar)
+
+// tratamento de erro!
+
+console.error('Erro!')
---
+++ JavaScript-BackEnd/Aula05/funcoes.js
@@ -0,0 +1,20 @@
+//let x = ''
+//console.log(x)
+//x = 'oi'
+
+// 1) declara função
+function imprimeTexto(texto) {
+  console.log(texto)
+}
+
+//2) executa a função (1 ou + vezes)
+
+imprimeTexto(soma())
+
+// três formas de escrever funções
+
+function soma() {
+  return 2 + 2
+}
+
+//console.log(soma())
