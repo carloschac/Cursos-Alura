4d30b3ec76ec129958b330751ee02113b646c17d
Carlos Henrique <carloss.chac@gmail.com>
2022-06-10 12:32:19 -0300

    commit

---
+++ JavaScript-BackEnd/Aula05/parametros.js
@@ -0,0 +1,22 @@
+//function soma(num1, num2) {
+//return num1 + num2
+//}
+
+//console.log(soma(2, 2))
+//console.log(soma(58, -96))
+//console.log(soma(10, -10))
+
+function nomeIdade(nome, idade) {
+  return `meu nome é ${nome} e minha idade é ${idade}`
+}
+
+//console.log(nomeIdade(40, 'Carlos'))
+
+function soma(num1, num2) {
+  return num1 + num2
+}
+
+function multiplica(numero1 = 1, numero2 = 1) {
+  return numero1 * numero2
+}
+console.log(multiplica(soma(4, 5)))
