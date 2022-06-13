c1756ae9ea76c439d0fa4dac009184183ed51d84
Carlos Henrique <carloss.chac@gmail.com>
2022-06-13 15:23:23 -0300

    Create programa.cs

---
+++ formacao-C/programa.cs
@@ -0,0 +1,15 @@
+using System;
+
+namespace PrimeiroPrograma
+{
+  class programa
+  {
+    static void Main(string[] args)
+    {
+      Console.WriteLine("Olá, mundo este é meu primeiro programa");
+
+      Console.WriteLine("A execução terminou, tecle enter para fianlizar . . . ");
+      Console.ReadLine();
+    }
+  }
+}
