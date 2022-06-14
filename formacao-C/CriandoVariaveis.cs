0543b6b95e5ac375317b5a576aaada701b2e8f3a
Carlos Henrique <carloss.chac@gmail.com>
2022-06-14 11:59:28 -0300

    Create CriandoVariaveis.cs

---
+++ formacao-C/CriandoVariaveis.cs
@@ -0,0 +1,20 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CriandoVariaveis
+{
+  class programa
+  {
+    static void Main(string[] args)
+    {
+      Console.WriteLine("Executando projeto 2");
+      double idade = 32;
+
+      Console.WriteLine("Execução finalizada. Tecle enter para sair . . .");
+      Console.ReadLine();
+    }
+  }
+}
