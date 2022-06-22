cf4892c58af4b33451608ffa28359e3fe427c9aa
Carlos Henrique <carloss.chac@gmail.com>
2022-06-22 16:47:37 -0300

    commit

--- JavaScript-BackEnd/Aula01-2/localizando-aluno.js
+++ JavaScript-BackEnd/Aula01-2/localizando-aluno.js
@@ -6,6 +6,15 @@ let listaDeNotasEAlunos = [alunos, mediasDosAlunos]
 
 const exibeNomeNota = nomeDoAluno => {
   if (listaDeNotasEAlunos[0].includes(nomeDoAluno)) {
-    indice = listaDeNotasEAlunos.indexOf(nomeDoAluno)
+    let indice = listaDeNotasEAlunos[0].indexOf(nomeDoAluno)
+    return (
+      listaDeNotasEAlunos[0][indice] +
+      ', sua média é ' +
+      listaDeNotasEAlunos[1][indice]
+    )
+  } else {
+    return 'Aluno não esta cadastrado'
   }
 }
+
+console.log(exibeNomeNota('Nemo'))
