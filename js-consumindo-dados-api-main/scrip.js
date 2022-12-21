67b605d3e619e4e8db129a692ae9f13c8b48a6b2
Carlos Henrique <carloss.chac@gmail.com>
2022-12-21 13:45:07 -0300

    Update scrip.js

---
+++ js-consumindo-dados-api-main/scrip.js
@@ -0,0 +1,11 @@
+console.log("Mensagem 1");
+
+function mandaMensagem() {
+  console.log("Mensagem 2 ");
+  console.log("Mensagem 3");
+  console.log("Mensagem4");
+}
+
+mandaMensagem();
+
+console.log("Mensagem pós function");
