33e65da316f1cf4bd6e54d6aa85228df19ae75dd
Carlos Henrique <carloss.chac@gmail.com>
2023-03-10 15:53:01 -0300

    commit

---
+++ php-orientacao-objetos/php-oo/src/Conta.php
@@ -0,0 +1,69 @@
+<?php 
+
+class Conta {
+
+private $titular;
+private int $saldo;
+private static $numeroDeContas = 0;
+
+public function __construct(Titular $titular)
+{
+    $this->titular = $titular;
+    $this->saldo = 0;
+
+    self::$numeroDeContas++;
+}
+
+public function __destruct()
+{
+    self::$numeroDeContas--;
+}
+
+public function sacar(float $valorASacar): void 
+{
+    if($valorASacar >$this->saldo) {
+        echo 'Saldo indisponível';
+        return;
+    } 
+        $this->saldo -= $valorASacar;
+}
+
+public function depositar(float $valorADepositar): void {
+    if($valorADepositar < 0) {
+        echo 'O valor precisa ser positivo';
+        return;
+    }
+        $this->saldo += $valorADepositar;
+}
+
+public function transferir(float $valorATransferir, Conta $contaDestino): void {
+    if ($valorATransferir > $this->saldo) {
+        echo "Saldo indisponível";
+        return;
+    } 
+        $this->sacar($valorATransferir);
+        $contaDestino->depositar($valorATransferir);
+}
+
+public function recuperaCpfTitular(): string
+{
+    return $this->titular->recuperaCpf();
+}
+
+public function recuperaNomeTitular(): string
+{
+    return $this->titular->recuperaNome();
+}
+
+public function recuperaSaldo(): float
+{
+    return $this->saldo;
+}
+
+
+ public static function recuperaNumeroConta(): int
+{
+    return self::$numeroDeContas;
+}
+
+}
